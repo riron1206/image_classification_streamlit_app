982eb11812221b2df125345d3940a6b19e26a207
riron1206 <rironyokoi@gmail.com>
2021-04-02 01:38:43 +0900

    add py from my_PC

--- git_push.bat
+++
@@ -1,8 +0,0 @@
-@rem ì¬“ú2021/4/2
-
-cd C:\Users\81908\MyGitHub\image_classification_streamlit_app
-call git add --all
-call git commit -m "add py from my_PC"
-call git push origin master
-
-pause
